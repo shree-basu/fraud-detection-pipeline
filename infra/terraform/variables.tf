3375c09e64b2559e1df24d3896922126300d89dd
Shreetama <basushreetama99@gmail.com>
2026-05-02 18:48:11 +0530

    feat: add variables.tf with project_id, region, dataset_id

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id"{
+    description = "The GCP project ID"
+    type = string
+}
+variable "region"{
+    description = "region of deployment"
+    type = string
+}
+variable "dataset_id"{
+    description = "id of the dataset in BQ"
+    type = string
+}
