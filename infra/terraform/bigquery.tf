b1c28b78bcb893c7268b6e70e717efd467705f1f
Shreetama <basushreetama99@gmail.com>
2026-05-07 07:01:35 +0530

    feat: add raw_transactions table schema

--- infra/terraform/bigquery.tf
+++ infra/terraform/bigquery.tf
@@ -3,3 +3,26 @@ resource "google_bigquery_dataset" "fraud_detection"{
     project = var.project_id
     location = var.region
 }
+resource "google_bigquery_table" "raw_transactions"{
+    dataset_id = google_bigquery_dataset.fraud_detection.dataset_id
+    table_id = "raw_transactions"
+    project = var.project_id
+
+    schema = jsonencode([
+        {name = "transaction_id" , type = "STRING"},
+        {name = "account_id" , type = "STRING"},
+        {name = "amount" , type = "FLOAT"},
+        {name = "currency" , type = "STRING"},
+        {name = "merchant_category" , type = "STRING"},
+        {name = "timestamp" , type = "TIMESTAMP"},
+        {name = "country_code" , type = "STRING"},
+        {name = "is_online" , type = "BOOLEAN"},
+        {name = "hour_of_day" , type = "INTEGER"},
+        {name = "is_high_risk_country" , type = "BOOLEAN"},
+        {name = "has_ip_address" , type = "BOOLEAN"},
+        {name = "fraud_signals" , type = "STRING"},
+        {name = "risk_score" , type = "INTEGER"},
+        {name = "is_fraud_alert" , type = "BOOLEAN"},
+        {name = "processed_at" , type = "TIMESTAMP"}
+        ])
+}
