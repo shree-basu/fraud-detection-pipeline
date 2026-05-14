cb93f17f2af29519b3faea38eb19d52f700d5552
Shreetama <basushreetama99@gmail.com>
2026-05-14 08:12:28 +0530

    add main.py constants

--- pipeline/main.py
+++ pipeline/main.py
@@ -5,3 +5,10 @@ from pipeline.transforms.validate import ValidateTransaction
 from pipeline.transforms.enrich import EnrichTransaction
 from pipeline.transforms.detect import DetectFraud
 from pipeline.utils.bq_utils import RAW_SCHEMA, FRAUD_SCHEMA
+
+PROJECT = "your-project-id"
+REGION = "us-central1"
+INPUT_LOGIC = f"projects/{PROJECT}/topics/fraud-transactions"
+DLQ_BUCKET = f"gs://{PROJECT}-dlq/invalid-transactions"
+RAW_TABLE = f"{PROJECT}: fraud_detection.raw_transactions"
+FRAUD_TABLE = f"{PROJECT}:fraud_detection.fraud_alerts"
