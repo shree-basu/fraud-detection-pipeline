a12342b9252385df3f4ce8ccc6c90ace7901485f
Shreetama <basushreetama99@gmail.com>
2026-05-14 06:35:49 +0530

    add RAW_SCHEMA for raw_transactions table

---
+++ pipeline/utils/bq_utils.py
@@ -0,0 +1,18 @@
+RAW_SCHEMA = [
+    {"name" = "transaction_id", "type"= "STRING"},
+    {"name" = "account_id", "type"= "STRING"},
+    {"name" = "amount", "type"= "FLOAT"},
+    {"name" = "currency", "type"= "STRING"},
+    {"name" = "merchant_category", "type"= "STRING"},
+    {"name" = "timestamp", "type"= "TIMESTAMP"},
+    {"name" = "country_code", "type"= "STRING"},
+    {"name" = "", "is_online"= "BOOLEAN"},
+    {"name" = "", "hour_of_day"= "INTEGER"},
+    {"name" = "", "is_high_risk_country"= "BOOLEAN"},
+    {"name" = "", "has_ip_address"= "BOOLEAN"},
+    {"name" = "", "fraud_signals"= "STRING"},
+    {"name" = "", "risk_score"= "INTEGER"},
+    {"name" = "", "is_fraud_alert"= "BOOLEAN"},
+    {"name" = "", "procesed_at"= "TIMESTAMP"},
+]
+
