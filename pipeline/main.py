71d73928bfb969b4fb706e91917c0c39ed00c962
Shreetama <basushreetama99@gmail.com>
2026-05-16 23:38:54 +0530

    add run function in main.py

--- pipeline/main.py
+++ pipeline/main.py
@@ -12,3 +12,16 @@ INPUT_LOGIC = f"projects/{PROJECT}/topics/fraud-transactions"
 DLQ_BUCKET = f"gs://{PROJECT}-dlq/invalid-transactions"
 RAW_TABLE = f"{PROJECT}: fraud_detection.raw_transactions"
 FRAUD_TABLE = f"{PROJECT}:fraud_detection.fraud_alerts"
+
+def run():
+    options = PipelineOptions(
+        project=PROJECT,
+        region=REGION,
+        runner='DataflowRunner',
+        streaming=True,
+        save_main_session=True,
+        job_name="fraud-detection-pipeline",
+        temp_location=f"gs://{PROJECT}-temp/dataflow"
+        staging_location=f"gs://{PROJECT}-staging/dataflow",
+    )
+    options.view_as(StandardOptions).streaming = True
